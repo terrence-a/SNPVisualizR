dfff578fca37cced8b8d42ff61a4ab38810d3777
Terrence Amponsah <terrence.amponsah@mail.utoronto.ca>
2021-11-20 22:32:12 -0500

    fixed tiTv bug, started vignettes and tests

--- R/calculateTiTV.R
+++ R/calculateTiTV.R
@@ -9,18 +9,23 @@
 #' @examples
 #' vcfData <- loadVCF("inst/examples/text000.vcf)
 #' calculateTiTv(vcfData)
+#'
+#' @references
+#' Wang, Jing et al. “Genome measures used for quality control are dependent on
+#' gene function and ancestry.” Bioinformatics (Oxford, England) vol. 31,3
+#' (2015): 318-23. doi:10.1093/bioinformatics/btu668
 
 ref_a <- alt_a <- NULL
 calculateTiTv <- function(vcfObj){
   #it's just the ratio of A<->G + C<->T changes / the rest.
   #literally could be written in 1 line i think this is the right way to
-  ti <- vcfObj@variantdata[
+  ti <- vcfObj@variantData[
     (ref_a == "A" & alt_a == "G") |
     (ref_a == "G" & alt_a == "A") |
     (ref_a == "T" & alt_a == "C") |
     (ref_a == "C" & alt_a == "T") ]
 
-  tv <- vcfObj@variantdata[
+  tv <- vcfObj@variantData[
       (ref_a == "A" & alt_a == "C") | (ref_a == "C" & alt_a == "A" )|
       (ref_a == "T" & alt_a == "A") | (ref_a == "A" & alt_a == "T" )|
       (ref_a == "G" & alt_a == "C") | (ref_a == "C" & alt_a == "G" )|
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(SNPVisualizR)
+
+test_check("SNPVisualizR")
